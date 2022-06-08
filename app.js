af25269b230312595e1734ad0e80fdbe7055c95c
Sagameister <86540738+Sagameister@users.noreply.github.com>
2022-06-09 00:54:33 +0200

    add ; on line 3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import './style/main.css'
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
 export default class Sketch {
     constructor(options) {
